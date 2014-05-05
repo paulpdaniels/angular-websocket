f14262f29746b60147323f761ad89f2a81cf3a2e
paulpdaniels <paulpdaniels@gmail.com>
2014-05-05 11:48:22 -0700

    Update ngWebSocket.js
    Added a simple switch for switching on requirejs

--- ngWebSocket.js
+++ ngWebSocket.js
@@ -1,35 +1,41 @@
-angular.module('ngWs', []).factory('$ws', function($q) {
-  
-  return {
-    connect : function(url, params, ext) {
-      var deferred = $q.defer();
-      
-      var url = url,
-          params = params || {},
-          ext = ext || [],
-          
-          query = _.isEmpty(params) ? '' :
-                  ('?' + _.chain(params).pairs().map(function(pair){
-                  
-                  
-                  }).value().join('&'));
-          
-          var ws = new WebSocket(url + query, ext);
-          
-          ws.onopen = function() {
-            deferred.resolve(ws);
-          };
-          
-          ws.onerror = function(e) {
-            deferred.reject(e);
-          };
-          
-          ws.onclose = function(r) {
-            deferred.rejected();
-          };
-          
-          return deferred.promise;
-    }
-  
-  };
+var def = define || function(name, deps, method) {
+  return method(angular);  
+};
+
+define('ngWebSocket', ['angular', 'underscore'], function(angular) {
+  return angular.module('ngWs', []).factory('$ws', function($q) {
+    
+    return {
+      connect : function(url, params, ext) {
+        var deferred = $q.defer();
+        
+        var url = url,
+            params = params || {},
+            ext = ext || [],
+            
+            query = _.isEmpty(params) ? '' :
+                    ('?' + _.chain(params).pairs().map(function(pair){
+                    
+                    
+                    }).value().join('&'));
+            
+            var ws = new WebSocket(url + query, ext);
+            
+            ws.onopen = function() {
+              deferred.resolve(ws);
+            };
+            
+            ws.onerror = function(e) {
+              deferred.reject(e);
+            };
+            
+            ws.onclose = function(r) {
+              deferred.rejected();
+            };
+            
+            return deferred.promise;
+      }
+    
+    };
+  });
 });
